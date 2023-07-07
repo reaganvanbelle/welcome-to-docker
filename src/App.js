8c6509fbcaf0a8f99d2b5970a6b440009d8a54c3
Reagan Van Belle <reaganvanbelle@gmail.com>
2023-07-07 10:25:57 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ const App = () => {
         <h1 style={{ marginBottom: "0px" }}>
           Congratulations!!!
         </h1>
-        <p style={{ marginTop: "10px", marginBottom: "50px" }}>
+        <p style={{ marginTop: "10px", marginBottom: "0px" }}>
           You ran your first container.
         </p>
+        <p style={{ marginTop: "5px", marginBottom: "50px"}}>
+          Franklin, I made this for you.
+        </p>
         <div>
-          <a target="_blank" href={"https://twitter.com/intent/tweet?text=" + shareMessage + "&url=" + shareLink} class="fa fa-twitter" rel="noopener noreferrer"> </a>
-          <a target="_blank" href={"https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink} class="fa fa-linkedin" rel="noopener noreferrer"> </a>
-          <a target="_blank" href={"https://reddit.com/submit?title=" + shareMessage + "&url=" + shareLink} class="fa fa-reddit" rel="noopener noreferrer"> </a>
+          <a target="_blank" href={"https://www.youtube.com/watch?v=NwTsZHGQ6FE" + shareMessage + "&url=" + shareLink} class="fa fa-twitter" rel="noopener noreferrer"> </a>
+          <a target="_blank" href={"https://www.youtube.com/watch?v=NwTsZHGQ6FE" + shareLink} class="fa fa-linkedin" rel="noopener noreferrer"> </a>
+          <a target="_blank" href={"https://www.youtube.com/watch?v=NwTsZHGQ6FE" + shareMessage + "&url=" + shareLink} class="fa fa-reddit" rel="noopener noreferrer"> </a>
         </div>
       </header>
     </div>
